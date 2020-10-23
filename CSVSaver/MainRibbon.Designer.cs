0bd8176b428b2d314a9923b8b99e2f1b2934087f
Андрей <jyuravlev@ts-most.ru>
2020-10-23 12:52:16 +0500

    Добавьте файлы проекта.

---
+++ CSVSaver/MainRibbon.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace CSVSaver
+{
+    partial class MainRibbon : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public MainRibbon()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.KTO = this.Factory.CreateRibbonGroup();
+            this.ImportToCSV = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.KTO.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.KTO);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // KTO
+            // 
+            this.KTO.Items.Add(this.ImportToCSV);
+            this.KTO.Label = "КТО";
+            this.KTO.Name = "KTO";
+            // 
+            // ImportToCSV
+            // 
+            this.ImportToCSV.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.ImportToCSV.Description = "Испортировать содержимео в CSV файл";
+            this.ImportToCSV.Image = global::CSVSaver.Properties.Resources.icons8_импорт_из_csv_50;
+            this.ImportToCSV.Label = "Импорт в CSV файл";
+            this.ImportToCSV.Name = "ImportToCSV";
+            this.ImportToCSV.ShowImage = true;
+            this.ImportToCSV.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.ImportToCSV_Click);
+            // 
+            // MainRibbon
+            // 
+            this.Name = "MainRibbon";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.KTO.ResumeLayout(false);
+            this.KTO.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup KTO;
+        private Microsoft.Office.Tools.Ribbon.RibbonButton ImportToCSV;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal MainRibbon Ribbon1
+        {
+            get { return this.GetRibbon<MainRibbon>(); }
+        }
+    }
+}
---
+++ CSVSaver/MainRibbon.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CSVSaver
+{
+    public partial class MainRibbon
+    {
+        //методы событий
+        /// <summary> Создание CSV файла с параметрами листов. </summary>
+        private void ImportToCSV_Click(object sender, RibbonControlEventArgs e)
+        {
+            try
+            {
+                var sheetData = GetActiveSheetDataArray();
+
+                string path = Globals.ThisAddIn.Application.ActiveWorkbook.Path + "\\CSV\\";
+                string name = Globals.ThisAddIn.Application.ActiveWorkbook.Name.Split('.').First() + "_" + GetInventory(sheetData) + ".csv";
+
+                if (!Directory.Exists(path))
+                    Directory.CreateDirectory(path);
+
+                File.WriteAllText(path + name, BuildingContent(sheetData), Encoding.GetEncoding("Windows-1251"));
+                MessageBox.Show(null, "Файл создан!", "Успех", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(null, "Ошибка при создании CSV файла:\r\n - " + ex.Message + "\r\n\r\nТрасировка ошибки:\r\n" + ex.StackTrace,
+                    "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        //вспомогательные методы
+        /// <summary> Получение инвентарного номера из листа. </summary>
+        private string GetInventory(object[][] sheetData)
+            => sheetData[1][1].ToString().Split('|')[2].Trim();
+
+        /// <summary> Формирование содержимого CSV файла. </summary>
+        private string BuildingContent(object[][] sheetData)
+            => string.Join("\r\n", sheetData.Select(x => ParseData(x)).Where(x => x != null)) + "\r\n";
+
+        /// <summary> Разбор и формирование строки для CSV. </summary>
+        private string ParseData(object[] rowData)
+        {
+            string ConvertToString(object source)
+               => source != null ? source.ToString() : string.Empty;
+            int ConvertToInt(object source)
+               => int.TryParse(ConvertToString(source), out int value) ? value : 0;
+
+            var name = ConvertToString(rowData[5]).ToLower();
+            if (name.Contains("лист"))
+            {
+                string position = ConvertToString(rowData[2]);
+                string thickness = new string(name.SkipWhile(x => x < '0' || x > '9').TakeWhile(x => x >= '0' && x <= '9').ToArray());
+                string count = (ConvertToInt(rowData[3]) + ConvertToInt(rowData[4])).ToString();
+                string metal = ConvertToString(rowData[6]);
+                return string.Join(";", new[] { position, count, thickness, metal });
+            }
+            else
+                return null;
+        }
+
+        /// <summary> Получение данных Excel листа. </summary>
+        private object[][] GetActiveSheetDataArray()
+        {
+            Range range = Globals.ThisAddIn.Application.ActiveSheet.UsedRange;
+
+            return Enumerable.Range(1, range.Rows.Count).Select(row
+                => Enumerable.Range(1, range.Columns.Count).Select(cell => (object)range.Value2[row, cell]).ToArray()
+            ).ToArray();
+        }
+    }
+}
